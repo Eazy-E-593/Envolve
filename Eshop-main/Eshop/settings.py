6a40ac9300b593e5f0265546d12816db128c60e1
Eazy-E-593 <wjcm.0604952291@gmail.com>
2024-01-20 14:34:24 -0500

    version2

--- Eshop-main/Eshop/settings.py
+++ Eshop-main/Eshop/settings.py
@@ -132,3 +132,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
